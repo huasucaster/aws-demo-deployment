f3a82e9a14690dbc596da959cf803457f6a67da4
Derek(Jianyu) Shen <huasucaster@gmail.com>
2021-08-28 14:46:59 -0500

    sadASd

---
+++ src/test/java/com/example/demoAWS/DemoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
